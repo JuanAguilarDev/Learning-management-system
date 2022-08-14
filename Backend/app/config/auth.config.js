8dd6a65e840e9e4abe7a01572223d6a09e113f27
JuanAguilarDev <Juan_aguilar1290@outlook.es>
2022-08-14 18:28:43 -0500

    User auth added

---
+++ Backend/app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "juan-secret-key"
+};
---
+++ Backend/app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  db: "mongodb+srv://juan:Hola1234@cluster0.xrh47rm.mongodb.net/?retryWrites=true&w=majority"
+};
